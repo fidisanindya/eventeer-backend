d40d703e4a309b1007f0776ac13eba559327193d
DandunGP <dandun.gigih.prakoso@gmail.com>
2023-05-09 11:56:51 +0700

    [fix] error notfound

---
+++ app/Providers/TelescopeServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Telescope\Telescope;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Telescope\IncomingEntry;
+use Laravel\Telescope\TelescopeApplicationServiceProvider;
+
+class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
+{
+
+}

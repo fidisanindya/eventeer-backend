8ff0a28fcc0ffc7dc3055a4228cbca0fdd9c7264
aqilahakmalia <aqilaakmalia98@gmail.com>
2023-09-13 16:24:30 +0700

    add API upcoming submission

--- routes/api.php
+++ routes/api.php
@@ -183,6 +183,7 @@ Route::prefix('notification')->group(function () {
 Route::prefix('submission')->group(function () {
     Route::middleware(['jwt.auth'])->group(function () {
         Route::get('/list_submission', [SubmissionController::class, 'get_list_submission']);
+        Route::get('/upcoming_submission', [SubmissionController::class, 'get_upcoming_submission']);
         Route::get('/detail-submission/{id}', [SubmissionController::class, 'getDetailSubmission']);
         Route::post('/assign-submission/{id}', [SubmissionController::class, 'assignSubmission']);
     });

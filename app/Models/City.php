9a5f3eee520c17c0b0ff1495fc587b8c5fa09e8b
aqilahakmalia <aqilaakmalia98@gmail.com>
2023-10-19 11:16:58 +0700

    update API detail feed

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Province;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +13,9 @@ class City extends Model
     protected $table = 'module_city';
     protected $primaryKey = 'id_city';
     protected $guarded = ['id_city'];
+
+    public function province()
+    {
+        return $this->belongsTo(Province::class, 'id_province', 'id_province');
+    }
 }
